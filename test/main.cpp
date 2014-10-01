4394c871a05b7af5d72f125c4e5cdba4e451490e
Jan Schlüter <github@jan-schlueter.de>
2014-10-01 15:46:54 +0200

    Added tests for jukebox serialization/deserialization and musly_jukebox_guessneighbors

--- test/main.cpp
+++ test/main.cpp
@@ -18,6 +18,7 @@
 #include "musly/musly.h"
 #include "tools.h"
 #include "idpool.h"
+#include "minilog.h"
 
 /** poor man's test framework */
 int FAILED = 0;
@@ -202,22 +203,36 @@ void generate_music(float* out, int length, unsigned int seed = 0) {
 void test_method(std::string method) {
     std::cout << "Testing method \"" << method << "\"..." << std::endl;
     musly_jukebox* box = musly_jukebox_poweron(method.c_str(), NULL);
-    float song[22050 * 30];
     musly_track* tracks[100];
     musly_trackid trackids[100];
     for (int i = 0; i < 100; i++) {
         tracks[i] = musly_track_alloc(box);
     }
+    float similarities[100];
+    float similarities2[100];
+    int num_neighbors_guessed;
+    musly_trackid candidates[20];
+    musly_trackid candidates2[20];
+    for (int i = 0; i < 20; i++) {
+        candidates[i] = 0;
+        candidates2[i] = 0;
+    }
 
+    // First, we create a jukebox
     REQUIRE( "initially empty", musly_jukebox_trackcount(box) == 0 );
 
+    // We generate some tracks to play with
+    float* song = new float[22050 * 30];
     for (int i = 0; i < 100; i++) {
         generate_music(song, 22050 * 30, 42*i);
         REQUIRE( "analyzed song", musly_track_analyze_pcm(box, song, 22050*30, tracks[i]) == 0);
     }
+    delete[] song;
 
+    // We initialize the jukebox
     REQUIRE( "set music style", musly_jukebox_setmusicstyle(box, tracks, 25) == 0 );
 
+    // We add 50 tracks with automatically generated ids
     REQUIRE( "added tracks", musly_jukebox_addtracks(box, tracks, trackids, 50, true) == 0 );
     REQUIRE( "track count 50", musly_jukebox_trackcount(box) == 50 );
     REQUIRE( "max seen 49", musly_jukebox_maxtrackid(box) == 49 );
@@ -225,6 +240,7 @@ void test_method(std::string method) {
         REQUIRE( "generated track ids", trackids[i] == i );
     }
 
+    // We add 40 more tracks with user-defined ids
     for (int i = 50; i < 90; i++) {
         trackids[i] = 50 + i*27%367;  // rather unordered without duplicates
     }
@@ -233,16 +249,33 @@ void test_method(std::string method) {
     REQUIRE( "track count 90", musly_jukebox_trackcount(box) == 90);
     REQUIRE( "max seen 1000", musly_jukebox_maxtrackid(box) == 1000);
 
-    float similarities[90];
+    // We check whether similarity and candidate computation work
     REQUIRE( "computed similarities", musly_jukebox_similarity(box, tracks[42], trackids[42], tracks, trackids, 90, similarities) == 0 );
+    num_neighbors_guessed = musly_jukebox_guessneighbors(box, trackids[30], candidates, 20);
+    REQUIRE( "guessed neighbors", (num_neighbors_guessed == -1) || (num_neighbors_guessed == 20) );
 
+    // We check whether they even work deterministically (they should)
+    REQUIRE( "re-computed similarities", musly_jukebox_similarity(box, tracks[42], trackids[42], tracks, trackids, 90, similarities2) == 0 );
+    for (int i = 0; i < 90; i++) {
+        REQUIRE( "consistent similarities", similarities[i] == similarities2[i] );
+    }
+    REQUIRE( "re-guessed neighbors", musly_jukebox_guessneighbors(box, trackids[30], candidates2, 20) == num_neighbors_guessed );
+    if (num_neighbors_guessed > 0) {
+        for (int i = 0; i < num_neighbors_guessed; i++) {
+            REQUIRE( "consistent neighbor candidates", candidates[i] == candidates2[i] );
+        }
+    }
+
+    // We add 10 more tracks with generated ids...
     REQUIRE( "added even more tracks", musly_jukebox_addtracks(box, &tracks[90], &trackids[90], 10, true) == 0 );
     REQUIRE( "track count 100", musly_jukebox_trackcount(box) == 100 );
     REQUIRE( "max seen 1010", musly_jukebox_maxtrackid(box) == 1010 );
     for (int i = 0; i < 10; i++) {
         REQUIRE( "generated track ids", trackids[90 + i] == 1001 + i );
     }
-
+    // ...and do some removals and adds to restore the original jukebox state,
+    // with possibly different ordering of the jukebox-internal index
+    // and the trackids of the first 30 tracks increased by 1011
     REQUIRE( "removed first 30 tracks", musly_jukebox_removetracks(box, trackids, 30) == 0 );
     REQUIRE( "track count 70", musly_jukebox_trackcount(box) == 70 );
     REQUIRE( "max seen 1010", musly_jukebox_maxtrackid(box) == 1010 );
@@ -256,20 +289,86 @@ void test_method(std::string method) {
         REQUIRE( "generated track ids", trackids[i] == 1011 + i );
     }
 
-    float similarities2[90];
+    // We check whether similarity and candidate computation yield the same results as before
     REQUIRE( "re-computed similarities", musly_jukebox_similarity(box, tracks[42], trackids[42], tracks, trackids, 90, similarities2) == 0 );
     for (int i = 0; i < 90; i++) {
         REQUIRE( "consistent similarities", similarities[i] == similarities2[i] );
     }
+    REQUIRE( "re-guessed neighbors", musly_jukebox_guessneighbors(box, trackids[30], candidates2, 20) == num_neighbors_guessed );
+    if (num_neighbors_guessed > 0) {
+        // the ids of the first 30 tracks have changed; we need to adapt `candidates`
+        for (int i = 0; i < num_neighbors_guessed; i++) {
+            if (candidates[i] < 30) {
+                candidates[i] += 1011;
+            }
+        }
+        // candidates are not required to be returned in a particular order,
+        // so if the internal jukebox index has changed, the order may change
+        std::sort(candidates, candidates + num_neighbors_guessed);
+        std::sort(candidates2, candidates2 + num_neighbors_guessed);
+        for (int i = 0; i < num_neighbors_guessed; i++) {
+            REQUIRE( "consistent neighbor candidates", candidates[i] == candidates2[i] );
+        }
+    }
 
+    // We export and import the jukebox state to/from a file
+    char *tmpfile = tmpnam(NULL);
+    REQUIRE( "exported jukebox state", musly_jukebox_tofile(box, tmpfile) > 0 );
+    musly_jukebox* box2;
+    REQUIRE( "imported jukebox state", (box2 = musly_jukebox_fromfile(tmpfile)));
+    remove(tmpfile);
+
+    // We check whether the re-imported jukebox is consistent with the original one
+    REQUIRE( "max seen 1040 (imported jukebox)", musly_jukebox_maxtrackid(box2) == 1040 );
+    REQUIRE( "computed similarities (imported jukebox)", musly_jukebox_similarity(box2, tracks[42], trackids[42], tracks, trackids, 90, similarities2) == 0 );
+    for (int i = 0; i < 90; i++) {
+        REQUIRE( "consistent similarities", similarities[i] == similarities2[i] );
+    }
+    REQUIRE( "guessed neighbors (imported jukebox)", musly_jukebox_guessneighbors(box2, trackids[30], candidates2, 20) == num_neighbors_guessed );
+    if (num_neighbors_guessed > 0) {
+        std::sort(candidates2, candidates2 + num_neighbors_guessed);
+        for (int i = 0; i < num_neighbors_guessed; i++) {
+            REQUIRE( "consistent neighbor candidates", candidates[i] == candidates2[i] );
+        }
+    }
+
+    // We check if the two jukeboxes are also consistent when adding new tracks
+    // (so the music style state has been exported and imported properly)
+    REQUIRE( "added 10 tracks to first jukebox", musly_jukebox_addtracks(box, &tracks[90], &trackids[90], 10, true) == 0 );
+    for (int i = 0; i < 10; i++) {
+        REQUIRE( "generated track ids", trackids[90 + i] == 1041 + i );
+    }
+    REQUIRE( "added 10 tracks to imported jukebox", musly_jukebox_addtracks(box2, &tracks[90], &trackids[90], 10, true) == 0 );
+    for (int i = 0; i < 10; i++) {
+        REQUIRE( "generated track ids", trackids[90 + i] == 1041 + i );
+    }
+    REQUIRE( "computed similarities (first jukebox)", musly_jukebox_similarity(box, tracks[10], trackids[10], tracks, trackids, 100, similarities) == 0 );
+    REQUIRE( "computed similarities (imported jukebox)", musly_jukebox_similarity(box2, tracks[10], trackids[10], tracks, trackids, 100, similarities2) == 0 );
+    for (int i = 0; i < 100; i++) {
+        REQUIRE( "consistent similarities", similarities[i] == similarities2[i] );
+    }
+    num_neighbors_guessed = musly_jukebox_guessneighbors(box, trackids[23], candidates, 20);
+    REQUIRE( "guessed neighbors (both jukeboxes or none)", musly_jukebox_guessneighbors(box2, trackids[23], candidates2, 20) == num_neighbors_guessed );
+    if (num_neighbors_guessed > 0) {
+        std::sort(candidates, candidates + num_neighbors_guessed);
+        std::sort(candidates2, candidates2 + num_neighbors_guessed);
+        for (int i = 0; i < num_neighbors_guessed; i++) {
+            REQUIRE( "consistent neighbor candidates", candidates[i] == candidates2[i] );
+        }
+    }
+
+    // Clean up whatever is left on the heap
     for (int i = 0; i < 100; i++) {
         musly_track_free(tracks[i]);
     }
     musly_jukebox_poweroff(box);
+    musly_jukebox_poweroff(box2);
 }
 
 
 int main() {
+    MiniLog::current_level() = logERROR;
+
     // Unit tests
     std::cout << "Components to test: unordered_idpool,ordered_idpool" << std::endl;
     test_unordered_idpool();
