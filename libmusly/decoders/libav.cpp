6330955f7a3e5d4361b74f450d8c73d23c905b85
Dominik Schnitzer <dominik@schnitzer.at>
2019-08-30 15:27:44 +0200

    libav: Fix use of flags in av_seek_frame()
    The bit-flags in av_seek_frame() were logically
    ||'d instead of bitwise |'d

--- libmusly/decoders/libav.cpp
+++ libmusly/decoders/libav.cpp
@@ -296,7 +296,7 @@ libav::decodeto_22050hz_mono_float(
         // fault when trying to access frame->data[i] for i > 0 further below)
         if ((excerpt_start > 0) && (av_seek_frame(fmtx, audio_stream_idx,
                     static_cast<int64_t>(static_cast<double>(excerpt_start) * st->time_base.den) / st->time_base.num,
-                    AVSEEK_FLAG_BACKWARD || AVSEEK_FLAG_ANY) >= 0)) {
+                    AVSEEK_FLAG_BACKWARD | AVSEEK_FLAG_ANY) >= 0)) {
             // skipping went fine: decode only what's needed
             decode_samples = static_cast<int>(excerpt_length * decx->sample_rate);
             excerpt_start = 0;
